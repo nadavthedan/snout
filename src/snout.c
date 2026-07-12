0c4aed0e7470f8b335d35c8be4430be2e5550d84
Nadav Dan <nadav.prod@gmail.com>
2026-07-12 23:15:32 +0300

    added some hook to the prerouting netfilter

--- src/snout.c
+++ src/snout.c
@@ -1,16 +1,41 @@
+#include <linux/printk.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nadev");
+MODULE_DESCRIPTION("A packet sniffer kernel module");
+
+static struct nf_hook_ops nfho;
+
+static unsigned int netfilter_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+    static unsigned long packet_count = 0;
+
+    if ((++packet_count % 1000) == 0) {
+        pr_info("Logged %lu packets!\n", packet_count);
+    }
+    return NF_ACCEPT;
+}
 
 static int __init snout_init(void) {
-  pr_info("snout init\n");
+  nfho.hook = netfilter_hook;
+  nfho.hooknum = NF_INET_PRE_ROUTING;
+  nfho.pf = PF_INET;
+  nfho.priority = NF_IP_PRI_FIRST;
+
+  nf_register_net_hook(&init_net, &nfho);
+
+  pr_info("snount init success\n");
   return 0;
 }
 
 static void __exit snout_exit(void) {
-  pr_info("snout exit\n");
+  nf_unregister_net_hook(&init_net, &nfho);
+
+  pr_info("snount exit success\n");
   return;
 }
 
