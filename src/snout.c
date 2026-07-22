6e671bffa3e42c8cff7d29ca98ec5be6cdf6dc1c
Nadav Dan <nadav.prod@gmail.com>
2026-07-22 19:58:16 +0300

    added a method to get a packet payload

--- src/snout.c
+++ src/snout.c
@@ -4,6 +4,10 @@
 #include <linux/module.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nadev");
@@ -11,13 +15,56 @@ MODULE_DESCRIPTION("A packet sniffer kernel module");
 
 static struct nf_hook_ops nfho;
 
-static unsigned int netfilter_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-    static unsigned long packet_count = 0;
+static bool get_packet_payload(struct sk_buff *skb, void *dest_buffer, int max_len, int *bytes_copied) {
+    struct iphdr *iph;
+    struct iphdr _iph;
+    int transport_offset = 0;
+    int payload_offset = 0;
+    int total_len = skb->len;
 
-    if ((++packet_count % 1000) == 0) {
-        pr_info("Logged %lu packets!\n", packet_count);
+    int net_offset = skb_network_offset(skb);
+    iph = skb_header_pointer(skb, net_offset, sizeof(struct iphdr), &_iph);
+    if (!iph) {
+        return false;
     }
+
+    transport_offset = skb_network_offset(skb);
+
+    if(iph->protocol == IPPROTO_TCP) {
+        struct tcphdr _tcph;
+        struct tcphdr *tcph;
+
+        tcph = skb_header_pointer(skb, transport_offset, sizeof(struct tcphdr), &_tcph);
+        if (!tcph) {
+            return false;
+        }
+
+        payload_offset = transport_offset + (tcph->doff * 4)
+    } else if (iph->protocol == IPPROTO_UDP) {
+        payload_offset = transport_offset + sizeof(struct udphdr);
+    } else {
+        return false;
+    }
+
+    if(pyalod_offset >= total_len) {
+        return false;
+    }
+
+    int available_payload_len = total_len - payload_offset;
+    int bytes_to_read = (available_payload_len < max_len) ? available_payload_len : max_len;
+
+    if (skb_copy_bits(skb, payload_offset, dest_buffer, bytes_to_read) < 0) {
+        return false;
+    }
+
+    *bytes_copied = bytes_to_read;
+    return true;
+}
+
+static unsigned int netfilter_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+
     return NF_ACCEPT;
+
 }
 
 static int __init snout_init(void) {
